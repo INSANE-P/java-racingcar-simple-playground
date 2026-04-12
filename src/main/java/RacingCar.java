0ab2e4ece72cb51520039ed3955bb027e225ad53
INSANE-P <chanbin0626@gmail.com>
2026-04-12 19:45:12 +0900

    refactor: RacingCar 불필요한 상속 제거

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,17 +1,25 @@
-public class RacingCar extends Car {
+public class RacingCar {
     private static final int MIN_MOVE_NUMBER = 4;
+    private final String name;
     private int position;
 
     public RacingCar(String name) {
-        super(name);
+        this.name = name;
         this.position = 0;
     }
 
+    public boolean canMove(int number) {
+        return number >= MIN_MOVE_NUMBER;
+    }
+
     public void moveForward(int randomNumber) {
-        setMoving(randomNumber >= MIN_MOVE_NUMBER);
-        if (isMoving()) {
+        if (canMove(randomNumber)) {
             position++;
         }
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getPosition() {
