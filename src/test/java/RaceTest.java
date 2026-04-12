6a59d224606041cb79b6b24ad378e12522e5cefb
INSANE-P <chanbin0626@gmail.com>
2026-04-12 21:51:25 +0900

    test: 구조 변경에 따른 테스트 수정

--- src/test/java/RaceTest.java
+++ src/test/java/RaceTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RaceTest {
+class RaceTest {
     @Test
     void 가장_멀리_이동한_베스트_드라이버가_우승한다() {
         RacingCar car1 = new RacingCar("찬빈");
@@ -16,7 +16,7 @@ public class RaceTest {
         car2.moveForward(3);
         car3.moveForward(2);
         car4.moveForward(1);
-        Race race = new Race(List.of(car1, car2, car3, car4));
+        Race race = new Race(new RacingCars(List.of(car1, car2, car3, car4)), new TestNumberGenerator(3));
         List<RacingCar> winners = race.findWinners();
 
         assertThat(winners.size()).isEqualTo(1);
@@ -36,9 +36,33 @@ public class RaceTest {
         car3.moveForward(2);
         car4.moveForward(1);
         car5.moveForward(4);
-        Race race = new Race(List.of(car1, car2, car3, car4, car5));
+        Race race = new Race(new RacingCars(List.of(car1, car2, car3, car4, car5)), new TestNumberGenerator(3));
         List<RacingCar> winners = race.findWinners();
 
         assertThat(winners.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 숫자가_4이상이면_모든_차가_전진한다() {
+        RacingCar car1 = new RacingCar("찬빈");
+        RacingCar car2 = new RacingCar("석준");
+        Race race = new Race(new RacingCars(List.of(car1, car2)), new TestNumberGenerator(4));
+
+        race.play(3);
+
+        assertThat(car1.getPosition()).isEqualTo(3);
+        assertThat(car2.getPosition()).isEqualTo(3);
+    }
+
+    @Test
+    void 숫자가_4미만이면_모든_차가_전진하지_않는다() {
+        RacingCar car1 = new RacingCar("상희");
+        RacingCar car2 = new RacingCar("혜림");
+        Race race = new Race(new RacingCars(List.of(car1, car2)), new TestNumberGenerator(3));
+
+        race.play(3);
+
+        assertThat(car1.getPosition()).isEqualTo(0);
+        assertThat(car2.getPosition()).isEqualTo(0);
     }
 }
--- src/test/java/RacingCarTest.java
+++ src/test/java/RacingCarTest.java
@@ -3,10 +3,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarTest {
     @Test
-    void 이름이_잘_지정된다() {
-        RacingCar car = new RacingCar("석준");
+    void 랜덤_숫자가_4_이상이면_레이싱카가_전진할수_있다() {
+        RacingCar car = new RacingCar("상희");
 
-        assertThat(car.getName()).isEqualTo("석준");
+        assertThat(car.canMove(4)).isTrue();
+    }
+
+    @Test
+    void 랜덤_숫자가_4보다_작으면_레이싱카가_전진할수_없다() {
+        RacingCar car = new RacingCar("혜림");
+
+        assertThat(car.canMove(3)).isFalse();
     }
 
     @Test
@@ -14,16 +21,14 @@ class RacingCarTest {
         RacingCar car = new RacingCar("상희");
         car.moveForward(4);
 
-        assertThat(car.isMoving()).isTrue();
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
     @Test
     void 랜덤_숫자가_4보다_작으면_레이싱카가_멈춘다() {
         RacingCar car = new RacingCar("혜림");
-        car.moveForward(2);
+        car.moveForward(3);
 
-        assertThat(car.isMoving()).isFalse();
         assertThat(car.getPosition()).isEqualTo(0);
     }
 }
